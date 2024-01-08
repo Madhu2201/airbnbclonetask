13d690e2564fae9891e317aefdf42a30da673d86
Madhu2201 <madhumathi2201@gmail.com>
2024-01-08 17:18:32 +0530

    link commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:4004/api/getproduct');
+      const response = await axios.get('https://airbnb-clone-sh2d.onrender.com/api/getproduct');
       setProducts(response.data);
     } catch (error) {
       console.error('Error fetching product data:', error);
